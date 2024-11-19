9b661dbf6579c694c7a068b633ca3aebf67c1968
Trainer <trainer@study9.com>
2024-11-19 21:41:40 +0530

    Update app route

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Docker Hub with GitHub Actions!"
+    return "Hello, Docker Hub with GitHub Actions! Updated version."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
